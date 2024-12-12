9832747d1dcc7a9d510b8b38f071123a9b7b0ce2
Lutow <118168487+Lutow@users.noreply.github.com>
2024-12-12 15:13:08 +0000

    Update EDA.py
    add numerical columns

--- EDA.py
+++ EDA.py
@@ -1,3 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+
+file_path = "Credit Risk Dataset.xlsx"  # Path to your uploaded file
+df = pd.read_excel(file_path)
+
+numerical_columns = df.select_dtypes(include=['float64', 'int64']).columns
+pd.set_option('display.max_columns', None)
