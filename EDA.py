e1521ff2c521720fed8c25fc743e566b9518bb2a
Lutow <118168487+Lutow@users.noreply.github.com>
2024-12-12 16:20:19 +0000

    Analysis of general statistics in EDA.py
    Compute Variance, mean, standard deviation...

--- EDA.py
+++ EDA.py
@@ -8,3 +8,12 @@ df = pd.read_excel(file_path)
 
 numerical_columns = df.select_dtypes(include=['float64', 'int64']).columns
 pd.set_option('display.max_columns', None)
+
+summary = pd.DataFrame({
+    'Mean': df[numerical_columns].mean(),
+    'Median': df[numerical_columns].median(),
+    'Variance': df[numerical_columns].var(),
+    'Standard Deviation': df[numerical_columns].std(),
+})
+
+numerical_columns = df.select_dtypes(include=['float64', 'int64']).columns
