8f520ec65eb20436bd2898a3c76fdd448eec841d
DJAMEL <45943775+djamel132@users.noreply.github.com>
2024-12-13 17:26:26 +0000

    ROC DecisionTree.py

--- DecisionTree.py
+++ DecisionTree.py
@@ -52,3 +52,15 @@ plt.title("Decision Tree")
 plt.show()
 
 print("",classification_report(y_test, y_pred, target_names=['Rejected', 'Approved']))
+
+fpr, tpr, thresholds = roc_curve(y_test, dt.predict_proba(X_test)[:, 1])
+roc_auc = auc(fpr, tpr)
+plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC Curve (AUC = {roc_auc:.2f})')
+plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--', label="No Skill (Random Guess)")
+plt.xlabel("False Positive Rate")
+plt.ylabel("True Positive Rate")
+plt.title("ROC Curve")
+plt.legend(loc="lower right")
+plt.grid()
+plt.show()
+
