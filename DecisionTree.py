49d18768bf7bbf2d6c1b2c32b83a98f11d0c774d
DJAMEL <45943775+djamel132@users.noreply.github.com>
2024-12-13 22:01:43 +0000

    Update DecisionTree.py

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.impute import KNNImputer
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
