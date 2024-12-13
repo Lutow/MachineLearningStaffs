faf2555614df18a45413df451841efd7b8ca66fa
DJAMEL <45943775+djamel132@users.noreply.github.com>
2024-12-13 17:35:37 +0000

    Update classification report and ROC in KNNTree.py

--- KNNTree.py
+++ KNNTree.py
@@ -56,3 +56,17 @@ plt.contourf(xx, yy, Z, cmap=ListedColormap(['#FFAAAA', '#AAAAFF']))
 plt.scatter(X_pca[:, 0], X_pca[:, 1], c=y, cmap=ListedColormap(['#FF0000', '#0000FF']), edgecolor='k' )
 plt.title(f"KNN Decision Boundaries (k={k})")
 plt.show()
+
+print("",classification_report(y_test, y_pred, target_names=['Rejected', 'Approved']))
+# Calcul ROC
+fpr, tpr, thresholds = roc_curve(y_test, knn.predict_proba(X_test)[:, 1])
+roc_auc = auc(fpr, tpr)
+#courbe ROC
+plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC Curve (AUC = {roc_auc:.2f})')
+plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--', label="No Skill (Random Guess)")
+plt.xlabel("False Positive Rate")
+plt.ylabel("True Positive Rate")
+plt.title("ROC Curve")
+plt.legend(loc="lower right")
+plt.grid()
+plt.show()
